dd4bd239fbd3b95cca62be6654cbd5304042f47b
Vincent Palomo <vincentpalomo@yahoo.com>
2024-02-28 16:32:07 -0600

    implemented add method in ll

--- misc_files/linked_list.js
+++ misc_files/linked_list.js
@@ -1,7 +1,7 @@
 class Node {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
   }
 }
 
@@ -10,5 +10,20 @@ class LinkedList {
     this.head = null;
     this.tail = null;
     this.length = 0;
+  }
+
+  add(data) {
+    const newNode = new Node(data);
+
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+
+    this.length++;
+    return this;
   }
 }
