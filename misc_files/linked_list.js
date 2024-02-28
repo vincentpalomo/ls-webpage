89484a01d4c36399db2171b15ec75767ead57f22
Vincent Palomo <vincentpalomo@yahoo.com>
2024-02-28 16:20:41 -0600

    implementing linked list

---
+++ misc_files/linked_list.js
@@ -0,0 +1,14 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
