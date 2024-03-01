fa8960f94c623888459f31459b5bee15edcd0296
Vincent Palomo <vincentpalomo@yahoo.com>
2024-02-29 21:46:50 -0600

    updated homepage

---
+++ src/app/components/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className="flex flex-col min-h-screen text-off-white bg-off-black overflow-hidden">
+      <div className="font-montserrat uppercase text-[11.25rem]">about</div>
+    </div>
+  );
+};
+
+export default About;
