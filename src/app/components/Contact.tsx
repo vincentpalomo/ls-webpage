49e2d11343b3f868214b3f8fd164582771ac0701
Vincent Palomo <vincentpalomo@yahoo.com>
2024-11-03 16:03:15 -0600

    fixed styling issue in contact section

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -9,7 +9,7 @@ const Contact = () => {
       className="min-h-screen bg-off-black text-off-white font-montserrat flex justify-center item-center"
       id="contact"
     >
-      <div className="w-[980px] flex flex-col items-center justify-between mt-20">
+      <div className="w-[980px] flex flex-col items-center justify-around mt-20">
         <div className="w-[980px] pointer-events-none">
           <div className="font-montserrat uppercase text-[128px] font-black">let&apos;s talk</div>
           {/* <div className="font-montserrat uppercase 2xl:text-[11.25rem] 2xl:leading-[180px] text-7xl sm:tracking-title xl:text-mac xl:leading-mac sm:ml-20">
@@ -29,7 +29,7 @@ const Contact = () => {
           </p>
         </div>
 
-        <div className="w-full pb-32">
+        <div className="w-full pb-32sty">
           <h1 className="uppercase text-[20px] font-bold">say hello</h1>
           <div>
             <ContactForm />
