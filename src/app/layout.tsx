568365f1940cc8eca927104c433a4cd3c0bbf7e1
Vincent Palomo <vincentpalomo@yahoo.com>
2024-11-11 17:02:33 -0600

    added favicon and is now showing on tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'Lucid Studio',
   description: 'A modern design studio',
+  icons: {
+    icon: '/images/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' href='/images/favicon.ico' sizes='any' />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
